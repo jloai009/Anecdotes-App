6d806272adc0938b83b826d6125bd71aa5037123
Jose <jloai009@fiu.edu>
2022-01-16 05:25:57 -0500

    Add functionality for adding anecdotes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { voteFor, createAnecdote } from './reducers/anecdoteReducer'
 import { useSelector, useDispatch } from 'react-redux'
 
 const App = () => {
@@ -7,6 +8,15 @@ const App = () => {
 
   const vote = (id) => {
     console.log('vote', id)
+    dispatch(voteFor(id))
+  }
+
+  const addAnecdote = (event) => {
+    event.preventDefault()
+    const content = event.target.anecdote.value
+    event.target.anecdote.value = ''
+    dispatch(createAnecdote(content))
+
   }
 
   return (
@@ -24,9 +34,11 @@ const App = () => {
         </div>
       )}
       <h2>create new</h2>
-      <form>
-        <div><input /></div>
-        <button>create</button>
+      <form onSubmit={addAnecdote}>
+        <div>
+          <input name="anecdote"/>
+        </div>
+        <button type="submit">create</button>
       </form>
     </div>
   )
