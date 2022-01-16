8e125fc6c0601e62f247aef373a7f1e1c2a109bb
Jose <jloai009@fiu.edu>
2022-01-16 05:48:16 -0500

    Add code to vote for and render anecdotes

---
+++ src/components/AnecdoteList.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { useSelector, useDispatch } from 'react-redux'
+import { voteFor } from '../reducers/anecdoteReducer'
+
+const AnecdoteList = (props) => {
+  const dispatch = useDispatch()
+  const anecdotes = useSelector(state => state)
+
+  const vote = (id) => {
+    console.log('vote', id)
+    dispatch(voteFor(id))
+  }
+  
+  return (
+    <div>
+    {anecdotes.map(anecdote =>
+      <div key={anecdote.id}>
+        <div>
+          {anecdote.content}
+        </div>
+        <div>
+          has {anecdote.votes}
+          <button onClick={() => vote(anecdote.id)}>vote</button>
+        </div>
+      </div>
+      )}
+    </div>
+  )
+}
+
+export default AnecdoteList
