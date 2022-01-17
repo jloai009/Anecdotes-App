ae9f289ae527d5bc8af78ea33d6aad2839429d43
Jose <jloai009@fiu.edu>
2022-01-16 22:34:15 -0500

    Use combined reducer

--- src/store.js
+++ src/store.js
@@ -1,10 +1,16 @@
 import React from 'react'
-import { createStore } from 'redux'
+import { createStore, combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers/anecdoteReducer'
+import notificationReducer from './reducers/notificationReducer'
+
+const combinedReducers = combineReducers({
+  anecdotes: reducer,
+  notification: notificationReducer
+})
 
 const store = createStore(
-  reducer,
+  combinedReducers,
   composeWithDevTools()
 )
 
