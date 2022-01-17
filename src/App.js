243733592fe434699a35197c537218a9a29f9303
Jose <jloai009@fiu.edu>
2022-01-16 23:11:26 -0500

    Render Filter Component

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Notification from './components/Notification'
+import Filter from './components/Filter'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 
@@ -7,6 +8,7 @@ const App = () => {
   return (
     <div>
       <h2>Anecdotes</h2>
+      <Filter />
       <Notification />
       <AnecdoteList />
       <AnecdoteForm />
