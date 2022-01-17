d8a97284dd5e0ec6a6bf6f4ebb9c4698839c73bc
Jose <jloai009@fiu.edu>
2022-01-16 23:10:05 -0500

    Use fiterReducer

--- src/store.js
+++ src/store.js
@@ -3,10 +3,12 @@ import { createStore, combineReducers } from 'redux'
 import { composeWithDevTools } from 'redux-devtools-extension'
 import reducer from './reducers/anecdoteReducer'
 import notificationReducer from './reducers/notificationReducer'
+import filterReducer from './reducers/filterReducer'
 
 const combinedReducers = combineReducers({
   anecdotes: reducer,
-  notification: notificationReducer
+  notification: notificationReducer,
+  filter: filterReducer
 })
 
 const store = createStore(
