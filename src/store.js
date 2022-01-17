9ff4c3cabb06dd91e32e3ef4726f56524440cd9f
Jose <jloai009@fiu.edu>
2022-01-16 20:37:03 -0500

    Add store.js

---
+++ src/store.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { createStore } from 'redux'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reducer from './reducers/anecdoteReducer'
+
+const store = createStore(
+  reducer,
+  composeWithDevTools()
+)
+
+export default store
