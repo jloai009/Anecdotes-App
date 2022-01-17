b94ffab7f3cabc63c3c5d2fa733c8112c68a7d61
Jose <jloai009@fiu.edu>
2022-01-17 03:38:46 -0500

    Initialize anecdotes with useEffect hook

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
+import anecdotesService from './services/anecdotes'
+import { initializeAnecdotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    anecdotesService
+      .getAll().then(anecdotes => dispatch(initializeAnecdotes(anecdotes)))
+  }, [dispatch])
   return (
     <div>
       <h1>Anecdotes</h1>
